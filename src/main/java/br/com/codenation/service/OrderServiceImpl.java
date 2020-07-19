e384dfd971c6ef9572db7d10e0a7b5ccc49ef0f5
Adriano Avelino <adriano-st777@hotmail.com>
2020-07-19 19:54:11 -0300

    atualiza os métodos para adicionar disconto

--- src/main/java/br/com/codenation/service/OrderServiceImpl.java
+++ src/main/java/br/com/codenation/service/OrderServiceImpl.java
@@ -13,57 +13,62 @@ import br.com.codenation.repository.ProductRepositoryImpl;
 
 public class OrderServiceImpl implements OrderService {
 
-	private ProductRepository productRepository = new ProductRepositoryImpl();
+    private ProductRepository productRepository = new ProductRepositoryImpl();
 
-	/**
-	 * Calculate the sum of all OrderItems
-	 */
-	@Override
-	public Double calculateOrderValue(List<OrderItem> items) {
-		return items.stream()
-    		.map(OrderItem::getProductId)
-    		.map(this::getProduct)
-    		.mapToDouble(Product::getValue)
-    		.sum();
-	}
+    /**
+     * Calculate the sum of all OrderItems
+     */
+    @Override
+    public Double calculateOrderValue(List<OrderItem> items) {
+        return items.stream()
+            .mapToDouble(this::getTotalByItem)
+            .sum();
+    }
+    
+    private Double getTotalByItem(OrderItem orderItem){
+        Optional<Product> optionalProduct = productRepository.findById(orderItem.getProductId());
+        Long quantity = orderItem.getQuantity();
+        if (optionalProduct.get().getIsSale()) {
+            return (optionalProduct.get().getValue() * quantity) * 0.8;
+        }
+        return optionalProduct.get().getValue() * quantity;
+    }
+    
+    private Product getProduct(Long id) {
+        Product p = new Product(0l, "", 0.0, false);
+        return productRepository.findById(id).orElse(p);
+    }
 
-	/**
-	 * Map from idProduct List to Product Set
-	 */
-	@Override
-	public Set<Product> findProductsById(List<Long> ids) {
+    /**
+     * Map from idProduct List to Product Set
+     */
+    @Override
+    public Set<Product> findProductsById(List<Long> ids) {
         return ids.stream()
             .map( id -> productRepository.findById(id))
             .filter(Optional::isPresent)
             .map(Optional::get).collect(Collectors.toSet());
-	}
+    }
 
-	/**
-	 * Calculate the sum of all Orders(List<OrderIten>)
-	 */
-	@Override
-	public Double calculateMultipleOrders(List<List<OrderItem>> orders) {
+    /**
+     * Calculate the sum of all Orders(List<OrderIten>)
+     */
+    @Override
+    public Double calculateMultipleOrders(List<List<OrderItem>> orders) {
         return orders.stream()
-                .map(List::stream)
-                .flatMap(orderItemStream -> orderItemStream)
-                .map(OrderItem::getProductId)
-                .map(this::getProduct)
-                .mapToDouble(Product::getValue)
-                .sum();
-	}
-	
-	private Product getProduct(Long id) {
-	    return productRepository.findById(id).get();
-	}
+            .mapToDouble(this::calculateOrderValue)
+            .sum();
+    }
 
-	/**
-	 * Group products using isSale attribute as the map key
-	 */
-	@Override
-	public Map<Boolean, List<Product>> groupProductsBySale(List<Long> productIds) {
-	    return productIds.stream()
-	        .map(this::getProduct)
-	        .collect(Collectors.groupingBy(Product::getIsSale));
-	}
+
+    /**
+     * Group products using isSale attribute as the map key
+     */
+    @Override
+    public Map<Boolean, List<Product>> groupProductsBySale(List<Long> productIds) {
+        return productIds.stream()
+            .map(this::getProduct)
+            .collect(Collectors.groupingBy(Product::getIsSale));
+    }
 
 }
